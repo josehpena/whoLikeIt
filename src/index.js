3b668a4f251f1916959f61b0787e816c4ad0c1f9
Jose Pena <josehpena2@gmail.com>
2023-05-24 22:12:45 -0300

    Second test case resolved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-const whoLikeIt = () => {
-    return "no one likes this"
+const whoLikeIt = (likes) => {
+    let whoLike = " likes this"
+
+    if(likes.length > 0)
+        whoLike =  likes[0] + whoLike
+    else 
+        whoLike = "no one" + whoLike
+
+    return whoLike
 }
 
 module.exports = {
