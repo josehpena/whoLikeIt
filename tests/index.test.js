d82d1e961265e923131243b71fcffc5cb8973100
Jose Pena <josehpena2@gmail.com>
2023-05-24 22:12:24 -0300

    Second test case with just one like

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,4 +4,8 @@ describe('whoLikeIt', () => {
     it('Should return "no one likes this" when no names are sended.', () => {
         expect(whoLikeIt([])).toBe("no one likes this");
     });
+    
+    it('Should return the name of the person when only one name is sent', () => {
+        expect(whoLikeIt(["Peter"])).toBe("Peter likes this");
+    });
 });
